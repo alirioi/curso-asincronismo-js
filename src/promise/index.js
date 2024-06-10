23f25c241cb763ccc320bc00e4064e122484c6d0
Alirio Isea <isealirio@gmail.com>
2024-06-09 21:54:51 -0400

    add: clase Promise

---
+++ src/promise/index.js
@@ -0,0 +1,22 @@
+const promise = new Promise(function (resolve, reject) {
+  resolve('Hey!');
+});
+
+const cows = 15;
+
+const countCows = new Promise(function (resolve, reject) {
+  if (cows > 10) {
+    resolve(`We have ${cows} cows on the farm`);
+  } else {
+    reject(`We don't have enough cows on the farm`);
+  }
+});
+
+countCows
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => console.log('Finally'));
